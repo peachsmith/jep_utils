28e60724f1a515b2ed8e6dc3d969d16129721a3a
John Powell <test@nonsense.com>
2018-12-01 09:50:55 -0600

    initial commit

---
+++ jep_utils.c
@@ -0,0 +1,11 @@
+#include "jep_utils.h"
+
+JEP_UTILS_API int test_func()
+{
+	return 1;
+}
+
+JEP_UTILS_API int add_one(int n)
+{
+	return n + 1;
+}
---
+++ jep_utils.h
@@ -0,0 +1,28 @@
+
+#ifdef _WIN32
+#ifdef JEP_UTILS_EXPORTS
+#define JEP_UTILS_API __declspec(dllexport)
+#else
+#define JEP_UTILS_API __declspec(dllimport)
+#endif
+#endif
+
+/**
+ * A test function
+ *
+ * Returns:
+ *   int - an integer
+ */
+JEP_UTILS_API int test_func(void);
+
+/**
+ * Another test function. This increments
+ * an integer.
+ *
+ * Params:
+ *   int - an integer to increment
+ *
+ * Returns:
+ *   int - the incremented integer
+ */
+JEP_UTILS_API int add_one(int n);
