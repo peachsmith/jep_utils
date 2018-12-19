30bd21595281a79cca4cb22fdd9ff4ff34c49bc3
John Powell <test@nonsense.com>
2018-12-18 19:13:12 -0600

    added byte buffer

--- jep_utils.c
+++ jep_utils.c
@@ -1,11 +1,57 @@
 #include "jep_utils.h"
 
-JEP_UTILS_API int test_func()
+JEP_UTILS_API jep_byte_buffer* JEP_UTILS_CALL
+jep_create_byte_buffer()
 {
-	return 1;
+
+	jep_byte_buffer* bb = NULL;
+
+	bb = (jep_byte_buffer*)malloc(sizeof(jep_byte_buffer));
+
+	if (bb == NULL)
+		return NULL;
+
+	bb->cap = 50;
+	bb->size = 0;
+	bb->buffer = (unsigned char*)malloc(sizeof(unsigned char));
+
+	if (bb->buffer == NULL)
+	{
+		free(bb);
+		return NULL;
+	}
+
+	return bb;
 }
 
-JEP_UTILS_API int add_one(int n)
+JEP_UTILS_API void JEP_UTILS_CALL
+jep_destroy_byte_buffer(jep_byte_buffer* bb)
 {
-	return n + 1;
+	if (bb == NULL)
+		return;
+
+	if (bb->buffer != NULL)
+		free(bb->buffer);
+
+	free(bb);
+}
+
+JEP_UTILS_API int JEP_UTILS_CALL
+jep_append_byte(jep_byte_buffer* bb, unsigned char b)
+{
+	if (bb == NULL || bb->buffer == NULL)
+		return 0;
+
+	if (bb->size >= bb->cap)
+	{
+		size_t new_size = bb->size + bb->size / 2;
+		bb->buffer = (unsigned char*)realloc(bb->buffer, new_size);
+	}
+
+	if (bb->buffer == NULL)
+		return 0;
+
+	bb->buffer[bb->size++] = b;
+
+	return 1;
 }
--- jep_utils.h
+++ jep_utils.h
@@ -1,3 +1,8 @@
+/**
+ * The main header file for jep_utils
+ */
+
+#include <stdlib.h>
 
 #ifdef _WIN32
 #ifdef JEP_UTILS_EXPORTS
@@ -5,28 +10,55 @@
 #else
 #define JEP_UTILS_API __declspec(dllimport)
 #endif
+#define JEP_UTILS_CALL __cdecl
+
 #elif defined(__linux__)
 #define JEP_UTILS_API
+#define JEP_UTILS_CALL
+
 #elif defined(__APPLE__)
 #define JEP_UTILS_API
+#define JEP_UTILS_CALL
+
 #endif
 
-/**
- * A test function
- *
- * Returns:
- *   int - an integer
- */
-JEP_UTILS_API int test_func(void);
+typedef struct jep_byte_buffer {
+
+	size_t size;
+	size_t cap;
+	unsigned char* buffer;
+
+}jep_byte_buffer;
 
 /**
- * Another test function. This increments
- * an integer.
- *
- * Params:
- *   int - an integer to increment
+ * Allocates resources for a new byte buffer.
+ * Returns NULL if memory allocation fails.
  *
  * Returns:
- *   int - the incremented integer
+ *   jep_byte_buffer - a new byte buffer
  */
-JEP_UTILS_API int add_one(int n);
+JEP_UTILS_API jep_byte_buffer* JEP_UTILS_CALL
+jep_create_byte_buffer();
+
+/**
+ * Frees resources allocated for a new byte buffer.
+ *
+ * Params:
+ *   jep_byte_buffer - the byte buffer to be destroyed
+ */
+JEP_UTILS_API void JEP_UTILS_CALL
+jep_destroy_byte_buffer(jep_byte_buffer* bb);
+
+/**
+ * Appends a byte to a byte buffer.
+ * Returns 1 on success or 0 on failure.
+ *
+ * Params:
+ *   jep_byte_buffer - the byte buffer to receive the new byte
+ *   unsigned char - the byte to be appended
+ *
+ * Returns:
+ *   int - an integer indicating success or failure
+ */
+JEP_UTILS_API int JEP_UTILS_CALL
+jep_append_byte(jep_byte_buffer* bb, unsigned char b);
