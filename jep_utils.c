1cdff15647eab9965cb41ca831f31a476f4e1312
John Powell <test@nonsense.com>
2018-12-21 13:29:08 -0600

    added char buffer

--- jep_utils.c
+++ jep_utils.c
@@ -1,5 +1,9 @@
 #include "jep_utils.h"
 
+/**********************
+* byte buffer         *
+**********************/
+
 JEP_UTILS_API jep_byte_buffer* JEP_UTILS_CALL
 jep_create_byte_buffer()
 {
@@ -65,6 +69,80 @@ jep_append_bytes(jep_byte_buffer* bb, unsigned char* b, int n)
 
 	for (i = 0; i < n; i++)
 		result += jep_append_byte(bb, b[i]);
+
+	return result;
+}
+
+
+/**********************
+* character buffer   *
+**********************/
+
+JEP_UTILS_API jep_char_buffer* JEP_UTILS_CALL
+jep_create_char_buffer()
+{
+
+	jep_char_buffer* cb = NULL;
+
+	cb = (jep_char_buffer*)malloc(sizeof(jep_char_buffer));
+
+	if (cb == NULL)
+		return NULL;
+
+	size_t cap = 50;
+
+	cb->cap = cap;
+	cb->size = 0;
+	cb->buffer = (char*)malloc(sizeof(char) * cap);
+
+	if (cb->buffer == NULL)
+	{
+		free(cb);
+		return NULL;
+	}
+
+	return cb;
+}
+
+JEP_UTILS_API void JEP_UTILS_CALL
+jep_destroy_char_buffer(jep_char_buffer* cb)
+{
+	if (cb == NULL)
+		return;
+
+	if (cb->buffer != NULL)
+		free(cb->buffer);
+
+	free(cb);
+}
+
+JEP_UTILS_API int JEP_UTILS_CALL
+jep_append_char(jep_char_buffer* cb, char c)
+{
+	if (cb == NULL || cb->buffer == NULL)
+		return 0;
+
+	if (cb->size >= cb->cap)
+	{
+		size_t new_size = cb->size + cb->size / 2;
+		cb->buffer = (char*)realloc(cb->buffer, new_size);
+	}
+
+	if (cb->buffer == NULL)
+		return 0;
+
+	cb->buffer[cb->size++] = c;
+
+	return 1;
+}
+
+JEP_UTILS_API int JEP_UTILS_CALL
+jep_append_chars(jep_char_buffer* cb, char* c, int n)
+{
+	int i, result = 0;
+
+	for (i = 0; i < n; i++)
+		result += jep_append_char(cb, c[i]);
 
 	return result;
 }
--- jep_utils.h
+++ jep_utils.h
@@ -30,6 +30,18 @@ typedef struct jep_byte_buffer {
 
 }jep_byte_buffer;
 
+typedef struct jep_char_buffer {
+
+	size_t size;
+	size_t cap;
+	char* buffer;
+
+}jep_char_buffer;
+
+/**********************
+* byte buffer         *
+**********************/
+
 /**
  * Allocates resources for a new byte buffer.
  * Returns NULL if memory allocation fails.
@@ -77,3 +89,56 @@ jep_append_byte(jep_byte_buffer* bb, unsigned char b);
  */
 JEP_UTILS_API int JEP_UTILS_CALL
 jep_append_bytes(jep_byte_buffer* bb, unsigned char* b, int n);
+
+
+/**********************
+ * character buffer   *
+ **********************/
+
+ /**
+  * Allocates resources for a new char buffer.
+  * Returns NULL if memory allocation fails.
+  *
+  * Returns:
+  *   jep_char_buffer - a new char buffer
+  */
+JEP_UTILS_API jep_char_buffer* JEP_UTILS_CALL
+jep_create_char_buffer();
+
+/**
+ * Frees resources allocated for a new char buffer.
+ *
+ * Params:
+ *   jep_char_buffer - the char buffer to be destroyed
+ */
+JEP_UTILS_API void JEP_UTILS_CALL
+jep_destroy_char_buffer(jep_char_buffer* bb);
+
+/**
+ * Appends a char to a char buffer.
+ * Returns 1 on success or 0 on failure.
+ *
+ * Params:
+ *   jep_char_buffer - the char buffer to receive the new char
+ *   char - the char to be appended
+ *
+ * Returns:
+ *   int - an integer indicating success or failure
+ */
+JEP_UTILS_API int JEP_UTILS_CALL
+jep_append_char(jep_char_buffer* bb, char b);
+
+/**
+ * Appends a char to a char buffer.
+ * Returns the number of chars added.
+ *
+ * Params:
+ *   jep_char_buffer - the char buffer to receive the new char
+ *   char* - a pointer to an array of chars
+ *   int - the number of chars to append
+ *
+ * Returns:
+ *   int - an integer indicating the number of chars added
+ */
+JEP_UTILS_API int JEP_UTILS_CALL
+jep_append_chars(jep_char_buffer* bb, char* b, int n);
